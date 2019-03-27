5f470b39a4d9f638aa750d81cbe4f5f1a488cf1e
yhseosbs <yh.seo.sbs@gmail.com>
2019-03-27 17:28:18 +0900

    PR Test 3

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Pull Request Test 2 <code>src/App.js</code> and save to reload.
+            Pull Request Test 3 <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
