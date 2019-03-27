6af5bd245420b09e1a96780ce191addccabb0992
Kevin Y. Seo <keviny.seo@gmail.com>
2019-03-27 13:58:25 +0900

    First Commit Test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            SBS TRD 2019 <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
